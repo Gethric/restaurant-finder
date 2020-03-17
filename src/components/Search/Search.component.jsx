3a2d7ee4ad568ea7d6568a6d117244a6071d6faa
Gethric <mangwisto@gmail.com>
2020-03-16 19:31:16 -0600

    removed unused files

--- src/components/Search/Search.component.jsx
+++ src/components/Search/Search.component.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import "./search.styles.scss";
 
 const Search = props => {
 	return (
 		<div className="search">
-			<input
-				type="text"
-				name="search"
-				placeholder="Find food!"
-				value={props.state.name}
-				onChange={props.handleChange}
-			/>
-
-			<button onClick={props.onClick}>Click me!</button>
+			<div className="tb">
+				<div className="td">
+					<input
+						type="text"
+						name="search"
+						placeholder="Find food!"
+						value={props.state.name}
+						onChange={props.handleChange}
+					/>
+				</div>
+				<div className="td" id="s-cover">
+					<button onClick={props.onClick}>
+						<div id="s-circle"></div>
+						<span></span>
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
